6830a6e11d81b1f797c75abc15bcd13e78d1364f
Mason.Lin <mason.lin1@hp.com>
2020-06-18 23:50:53 +0800

    add ROBOT_OPTIONS & --debugfile

--- robot_dryrun/run.bat
+++ robot_dryrun/run.bat
@@ -1,9 +1,12 @@
+set ROBOT_OPTIONS="--dryrun"
 set ROBOT_OUTPUTDIR=results_dryrun
 set ROBOT_SYSLOG_FILE=.\%ROBOT_OUTPUTDIR%\syslog.txt
 set ROBOT_SYSLOG_LEVEL=TRACE
-robot --dryrun --loglevel TRACE:INFO --outputdir %ROBOT_OUTPUTDIR% --pythonpath . test-case
+robot --debugfile debug.txt --loglevel TRACE:INFO --outputdir %ROBOT_OUTPUTDIR% --pythonpath . test-case
 
+set ROBOT_OPTIONS=
 set ROBOT_OUTPUTDIR=results
 set ROBOT_SYSLOG_FILE=.\%ROBOT_OUTPUTDIR%\syslog.txt
 set ROBOT_SYSLOG_LEVEL=TRACE
-robot --loglevel TRACE:INFO --outputdir %ROBOT_OUTPUTDIR% --pythonpath . test-case
+robot --debugfile debug.txt --loglevel TRACE:INFO --outputdir %ROBOT_OUTPUTDIR% --pythonpath . test-case
+
