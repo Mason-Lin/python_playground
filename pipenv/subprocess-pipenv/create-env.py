3e6a9096824d9bf1605cfdb2f539d6d1ff598b55
Mason.Lin <mason.lin1@hp.com>
2020-09-26 01:06:41 +0800

    create venv at home

--- pipenv/subprocess-pipenv/create-env.py
+++ pipenv/subprocess-pipenv/create-env.py
@@ -1,20 +1,34 @@
 import subprocess
+from pathlib import Path
 import time
 
 INFO_PRINTED = 251
+
+dryrun_venv = Path().home().joinpath("dryrun", "venv")
+dryrun_venv.mkdir(exist_ok=True)
+print(f"using venv for dryrun: {dryrun_venv}")
 
 
 def create_venv():
     start_time = time.time()
     # pipenv run pip install --no-index --find-links=wheels -r .\wheels\requirements.txt
     # subprocess.run("pipenv install --skip-lock", check=True)
-    subprocess.run("pipenv run pip install -U -r requirements.txt", check=True)
+    subprocess.run(
+        "pipenv run pip install -U -r requirements.txt",
+        check=True,
+        cwd=dryrun_venv,
+    )
     end_time = time.time()
     print(end_time - start_time)
 
 
 start_time = time.time()
-p = subprocess.run("pipenv run robot --dryrun testcase.robot", check=False)
+test_case = Path(__file__).absolute().parent.joinpath("testcase.robot")
+print(test_case)
+
+p = subprocess.run(
+    f"pipenv run robot --dryrun {test_case}", check=False, cwd=dryrun_venv
+)
 if p.returncode == 0:
     print("dryrun pass")
 elif 0 < p.returncode < INFO_PRINTED:
@@ -24,5 +38,6 @@ elif INFO_PRINTED <= p.returncode <= 255:
 else:
     print(f"something wrong returncode:{p.returncode}")
     create_venv()
+
 end_time = time.time()
 print(end_time - start_time)
