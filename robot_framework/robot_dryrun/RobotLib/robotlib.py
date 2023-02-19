7c73b707876a8d22a6e69e9c32b6141466b429ad
Christian Clauss <cclauss@me.com>
2023-02-19 17:25:28 +0100

    Update robotlib.py

--- robot_framework/robot_dryrun/RobotLib/robotlib.py
+++ robot_framework/robot_dryrun/RobotLib/robotlib.py
@@ -7,7 +7,7 @@ def this_keyword_exist_in_robotlib(message):
     logging.debug(message)
     logging.error(message)
 
-    pythonlib.this_keyword_exist_in_pythonlib('hello pythonlib')  # noqa: F821
+    pythonlib.this_keyword_exist_in_pythonlib('hello pythonlib')
 
-    this_keyword_not_exist_in_robotlib()
+    this_keyword_not_exist_in_robotlib()  # noqa: F821
     return 'OK'
